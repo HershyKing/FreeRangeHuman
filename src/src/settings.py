4b744d3e243ec85f2216f8f80755016b7f135b9b
Jason Tran <jtran@Jasons-MBP.fios-router.home>
2017-11-29 02:22:16 -0500

    Fixed accidental database change

--- src/src/settings.py
+++ src/src/settings.py
@@ -76,12 +76,8 @@ WSGI_APPLICATION = 'src.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'DB_NAME',
-        'USER': 'DB_USER',
-        'PASSWORD': 'DB_PASSWORD',
-        'HOST': 'localhost',   # Or an IP Address that your DB is hosted on
-        'PORT': '3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
