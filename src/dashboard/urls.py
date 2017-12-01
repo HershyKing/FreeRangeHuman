605d66309c35fbc3adb8e1149e3b2514f1b7fbdc
anishshah97 <tug04041@temple.edu>
2017-12-01 06:40:30 -0500

    Allowed for tags to be seen; working on user authentication next

--- src/dashboard/urls.py
+++ src/dashboard/urls.py
@@ -1,8 +1,8 @@
 from django.conf.urls import url
-
-from . import views
-
+from django.contrib import admin
+from dashboard import views
 
 urlpatterns = [
-
+	url(r'^$', views.index, name='index'),
+	url(r'tags', views.TagListView.as_view(), name='tags'),
 ]
